a0c35589fe997ad513e794c126f216926743716f
Charles Short <zulcss@gmail.com>
2023-04-26 11:34:40 -0400

    make script a bit smarter
    Signed-off-by: Charles Short <zulcss@gmail.com>

--- create-repo.sh
+++ create-repo.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-repo=ostree_repo
-ostree --repo=$repo init --mode=archive
+# $1 - path to repo 
+
+dir=$1
+if [ -z $dir ]; then
+	echo "You didnt supply a directory"
+	exit 1
+fi
+
+repo=$(dirname $dir)
+if [ ! -d $repo ]; then
+	mkdir -p $repo
+fi
+echo "Creating repo in $dir"
+ostree --repo=$dir init --mode=archive
+
